9ec4558fca17fb8b384f4745e9c5a0751e4a731f
Andrey Smirnov <andrew_1024@mail.ru>
2019-03-10 11:36:00 +0300

    transfer to orm

--- src/Models/File.php
+++ src/Models/File.php
@@ -31,24 +31,3 @@ class File extends Illuminate\Database\Eloquent\Model
     }
 
 }
-//{
-//    public function saveToDb($lastUserId, $fileName)
-//    {
-//        $STH = self::$pdo->prepare("INSERT INTO photos (user_id, image) values (:user_id, :image)");
-//        $STH->execute(['user_id' => $lastUserId, 'image' => $fileName]);
-//    }
-//
-//    public function loadImage($image, $id)
-//    {
-//        $STH = self::$pdo->prepare("INSERT INTO photos (user_id, image) values (:user_id, :image)");
-//        $STH->execute(['user_id' => $id, 'image' => $image]);
-//    }
-//
-//    public function getImages($id)
-//    {
-//        $imageList = self::$pdo->prepare("SELECT users.name, photos.image from photos LEFT JOIN users on users.id = photos.user_id where users.id =:user_id");
-//        $imageList->execute(['user_id' => $id]);
-//        $imageListResult = $imageList->fetchAll(PDO::FETCH_ASSOC);
-//        return $imageListResult;
-//    }
-//}
